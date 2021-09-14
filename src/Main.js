2248e87fffff49e48b676290c9bb9857877944a5
Bayarbileg Bayarsaikhan <bbayarbileg@gmail.com>
2021-09-14 19:04:06 +0200

    1st commit

---
+++ src/Main.js
@@ -0,0 +1,77 @@
+import React from "react";
+import './App.css';
+import fetchGraphQL from './fetchGraphQL';
+
+<div class="container-fluid pb-3">
+  <div class="d-grid gap-3" style="grid-template-columns: 1fr 2fr;">
+    <div class="bg-light border rounded-3">
+      <br><br><br><br><br><br><br><br><br><br>
+    </div>
+    <div class="bg-light border rounded-3">
+      
+<h1>Sites</h1>
+<table class="table table-striped table-hover table-bordered table-sm">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Municipality</th>
+      <th scope="col">Lot</th>
+      <th scope="col">Address</th>
+      <th scope="col">Parcel No.</th>
+      <th scope="col">Site Type</th>
+      <th scope="col">Site Contact</th>
+      <th scope="col">Telephone</th>
+      <th scope="col">Mail</th>
+      <th scope="col">Site Acquisition Manager</th>
+      <th scope="col">SID Site</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Schupfart</td>
+      <td>1</td>
+      <td>Aufgentweg 383</td>
+      <td>359</td>
+      <td>EFH</td>
+      <td>Sandrine Gardon</td>
+      <td>062 871 99 10</td>
+      <td>pfiffer.a@gmail.com</td>
+      <td>Alina Pfiffer</td>
+      <td>SID004000GNCJVD</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Schupfart</td>
+      <td>1</td>
+      <td>Aufgentweg 383</td>
+      <td>359</td>
+      <td>EFH</td>
+      <td>Sandrine Gardon</td>
+      <td>062 871 99 10</td>
+      <td>pfiffer.a@gmail.com</td>
+      <td>Alina Pfiffer</td>
+      <td>SID004000GNCJVD</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Schupfart</td>
+      <td>1</td>
+      <td>Aufgentweg 383</td>
+      <td>359</td>
+      <td>EFH</td>
+      <td>Sandrine Gardon</td>
+      <td>062 871 99 10</td>
+      <td>pfiffer.a@gmail.com</td>
+      <td>Alina Pfiffer</td>
+      <td>SID004000GNCJVD</td>
+    </tr>
+  </tbody>
+</table> 
+      
+      
+      <br><br><br><br><br><br><br><br><br><br>
+      
+    </div>
+  </div>
+</div>
---
+++ src/Window.js
@@ -0,0 +1,16 @@
+// your-app-name/src/App.js
+import React from 'react';
+import './App.css';
+import fetchGraphQL from './fetchGraphQL';
+
+function Window() {
+  // Render "Loading" until the query completes
+  return (
+    <div className="window">
+
+      <div class="b-example-divider"></div>
+    </div>
+  );
+}
+
+export default Window;
