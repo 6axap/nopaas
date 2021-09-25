68da6fa08085bb73bfc0890c8d41938573afb30a
Bayarbileg Bayarsaikhan <bbayarbileg@gmail.com>
2021-09-25 21:14:01 +0200

    data fetch via axios working

--- src/table.js
+++ src/table.js
@@ -1,60 +1,73 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import './style/App.css';
+import TableRow from "./tableRow";
 
 function Table() {
-  const [data, setData] = useState({});
+  const [state, setState] = useState({ data: [] });
 
+  // const callAPI = async () => {
+  //   await fetch('http://localhost:3001/sites', {
+  //     method: 'GET',
+  //     body: 'json'
+  //   })
+  //     .then(res => JSON.parse(res))
+  //     .then(res => {
+  //       const obj = res;
+  //       setState({
+  //         data: [obj]
+  //       });
+  //       console.log(state.data)
+  //     })
+  //     .catch(err => {
+  //       console.error(err)
+  //     });
+  // }
   useEffect(() => {
-    callAPI();
-  }, []); // see above https://reactjs.org/docs/hooks-effect.html#next-steps
+    getData();
+  }, []);
 
-  const callAPI = async () => {
-    await fetch('http://localhost:3001/sites')
-      .then(res => res.json())
-      .then(res => {
-        setData(res)
-        console.log(data)
-      })
-      .catch(err => {
-        console.error(err)
-      });
+  async function getData() {
+    try {
+      const response = await axios.get('http://localhost:3001/sites');
+      let data = response.data;
+      setState(({
+        data: data
+      }));
+    } catch (error) {
+      console.error(error);
+    }
   }
 
+  async function tableRows() {
+    state.data.forEach((row, index) => {
+    return (
+      <TableRow data={row} index={index + 1} />
+    );
+  })}
 
   return (
     <>
       <h1>Sites</h1>
-      <table class="table table-striped table-hover table-bordered table-sm">
+      <table className="table table-striped table-hover table-bordered table-sm">
       <thead>
           <tr>
           <th scope="col">#</th>
-          <th scope="col">Municipality</th>
           <th scope="col">Lot</th>
-          <th scope="col">Address</th>
-          <th scope="col">Parcel No.</th>
-          <th scope="col">Site Type</th>
-          <th scope="col">Site Contact</th>
-          <th scope="col">Telephone</th>
-          <th scope="col">Mail</th>
-          <th scope="col">Site Acquisition Manager</th>
           <th scope="col">SID Site</th>
+          <th scope="col">Status</th>
+          <th scope="col">Site Type</th>
+          <th scope="col">Address</th>
+          <th scope="col">City</th>
+          <th scope="col">Municipality</th>
+          <th scope="col">HAK Type</th>
+          <th scope="col">Offices</th>
+          <th scope="col">Appartments</th>
           </tr>
       </thead>
       <tbody>
-          <tr>
-          <th scope="row">1</th>
-          <td>Schupfart</td>
-          <td>1</td>
-          <td>Aufgentweg 383</td>
-          <td>359</td>
-          <td>EFH</td>
-          <td>Sandrine Gardon</td>
-          <td>062 871 99 10</td>
-          <td>pfiffer.a@gmail.com</td>
-          <td>Alina Pfiffer</td>
-          <td>SID004000GNCJVD</td>
-          </tr>
+          {tableRows}
+          {console.log(tableRows)}
       </tbody>
       </table> 
     </>
